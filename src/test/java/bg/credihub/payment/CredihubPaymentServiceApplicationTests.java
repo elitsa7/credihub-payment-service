4a9b3fc0993bc01c26bb36648f4c6507b7f089b0
Elitsa Dacheva <122827000+elitsa7@users.noreply.github.com>
2026-07-16 11:49:16 +0300

    Add application test properties

--- src/test/java/bg/credihub/payment/CredihubPaymentServiceApplicationTests.java
+++ src/test/java/bg/credihub/payment/CredihubPaymentServiceApplicationTests.java
@@ -2,8 +2,10 @@ package bg.credihub.payment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CredihubPaymentServiceApplicationTests {
 
 	@Test
